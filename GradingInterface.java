70921c806c0cacc5f47bfd3726f65577193be4ea
abbysandrews <aandrews@oberlin.edu>
2017-04-19 15:02:43 -0400

    Create GradingInterface.java
    interface for evaluating generated tweet. still need to add code for actually generating the tweet

---
+++ GradingInterface.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+/**
+ *
+ * @author abbysandrews
+ */
+public class GradingInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.print("Give me a hashtag: ");
+        Scanner s = new Scanner(System.in);
+        String hashtag = s.next();
+        System.out.print("Give me a mood (happy, sad, angry, troll): ");
+        String mood = s.next();
+        Boolean generated = false;
+        while(generated == false){
+            if(mood.equals("happy")){
+                //generate and print happy tweet
+                generated = true;
+            }
+            else if(mood.equals("sad")){
+                //generate and print sad tweet
+                generated = true;
+            }
+            else if(mood.equals("angry")){
+                //generate and print angry tweet
+                generated = true;
+            }
+            else if(mood.equals("troll")){
+                //generate and print troll tweet
+                generated = true;
+            }
+            else{
+                System.out.print("Please pick happy, sad, angry, or troll: ");
+                mood = s.next();
+            }
+        }
+        
+        Boolean good = false;
+        System.out.print("Do you like this tweet? (yes or no): ");
+        String yn = s.next();
+        if(yn.equals("yes")){
+            good = true;
+        }
+        
+    }
+    
+}
