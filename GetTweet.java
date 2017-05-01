52f153ea2ebb66043827396caa096aba8052cdad
rperezpadilla <mperezpa@oberlin.edu>
2017-04-30 20:47:41 -0400

    Added write file function; now does all from 4/25

--- GetTweet.java
+++ GetTweet.java
@@ -4,14 +4,18 @@
  * @author rperezpadilla Rita M. Perez-Padilla
  */
 
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+import java.util.List;
+
 import twitter4j.*;
-import twitter4j.TwitterException;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class GetTweet {
 	String ht; // Hashtag
-	ArrayList<Tweet> tweets;
+	List<Status> tweetsStatus;
 	
 	/**
 	 * Constructor class - gets tweets based on search for hashtag
@@ -19,45 +23,65 @@ public class GetTweet {
 	 */
 	public GetTweet(String hashtag) {
 		ht = hashtag;
-		tweets = new ArrayList<Tweet>(100);
-		// cool
 		
+		// Opens configuration stream to twitter
+		// all of these codes are from an account I made, @RAPATweets
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setOAuthConsumerKey("xBE3hpTKtWqKhVcLJIBAvH3g9");
 		cb.setOAuthConsumerSecret("cOaejr3YwtICFVtpe7thL7saPCc2G2iEVCXZn4gG5bfXQw11B7");
 		cb.setOAuthAccessToken("852351824166309897-ALtIPuCIKQAWHXLk7kE8bep4QfvNAtQ");
 		cb.setOAuthAccessTokenSecret("rqGNUEuJidfVk1B1c4plum5jqa3mdAKP25j3r5l39CZNC");
 
-		  Twitter twitter = new TwitterFactory(cb.build()).getInstance();
-		  Query query = new Query(hashtag);
-		  query.setCount(100);
+		// gets the twitter factory 
+		Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+		Query query = new Query(hashtag);
+		query.setCount(100); // gets the maximum of 100 tweets
 
-		  try {
-		    QueryResult result = twitter.search(query);
-		    ArrayList tweets = (ArrayList) result.getTweets();
+		try {
+			QueryResult result = twitter.search(query);
+			tweetsStatus = result.getTweets();
+			
+			// FOR TESTING: uncomment everything here to print every tweet
+//			for (int i = 0; i < tweetsStatus.size(); i++) {
+//				Status st = tweetsStatus.get(i);
+//				
+//				// for testing/print statements
+//				String user = st.getUser().getScreenName();
+//				String msg = st.getText();
+//				
+//				
+//				// prints all tweets: for testing
+//				System.out.println("USER: " + user + " wrote: " + msg);
+//			}
+		}
 
-		    for (int i = 0; i < tweets.size(); i++) {
-		      Status t = (Status) tweets.get(i);
-		      String user = t.getUser().getScreenName();
-		      String msg = t.getText();
-
-		      System.out.println("USER: " + user + " wrote: " + msg);
-		    }
-		  }
-
-		  catch (Throwable te) {
-		    System.out.println("Couldn't connect: " + te);
-		  };
-		
+	  catch (Throwable te) {
+		  System.out.println("Couldn't connect: " + te);
+	  };
+		  
 	}
 	
 	/**
 	 * Writes file with 1 tweet / line
 	 */
 	public void writeFile() {
-		// 1 tweet per line
+		Iterator<Status> tweetIterator = tweetsStatus.iterator();
 		
-		
+		try {
+			PrintWriter writer = new PrintWriter("savedtweets.txt", "UTF-8");
+			
+			while (tweetIterator.hasNext()) {
+				// print the text of each tweet, as it appears originally (INCLUDES emojis, links, etc)
+				Status curTweet = tweetIterator.next();
+				writer.println(curTweet.getText());
+			}
+			writer.close();
+			System.out.println("File written successfully.");
+			
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			System.out.println("Something went wrong in PrintWriter from GetTweet.java!");
+			e.printStackTrace();
+		}
 	}
 	
 	/**
